d2cde30a0f44db20dd3eee2a5a280511badea5ad
kbaba1001 <kbaba1001@users.noreply.github.com>
2018-06-21 13:11:06 +0900

    create test environment

--- dev/src/test.clj
+++ dev/src/test.clj
@@ -1,4 +1,5 @@
-(ns dev
+(ns test
+  (:refer-clojure :exclude [test])
   (:require [clojure.repl :refer :all]
             [fipp.edn :refer [pprint]]
             [clojure.tools.namespace.repl :refer [refresh]]
@@ -6,6 +7,7 @@
             [clojure.java.jdbc :as jdbc]
             [duct.core :as duct]
             [duct.core.repl :as duct-repl]
+            [eftest.runner :as eftest]
             [integrant.core :as ig]
             [integrant.repl :refer [clear halt go init prep reset]]
             [integrant.repl.state :refer [config system]]))
@@ -13,7 +15,10 @@
 (duct/load-hierarchy)
 
 (defn read-config []
-  (duct/read-config (io/resource "dev.edn")))
+  (duct/read-config (io/resource "test.edn")))
+
+(defn run-test []
+  (eftest/run-tests (eftest/find-tests "test")))
 
 (defn db []
   (-> system (ig/find-derived-1 :duct.database/sql) val))
@@ -21,9 +26,6 @@
 (defn q [sql]
   (jdbc/query (:spec (db)) sql))
 
-(clojure.tools.namespace.repl/set-refresh-dirs "dev/src" "src")
-
-(when (io/resource "local.clj")
-  (load "local"))
+(clojure.tools.namespace.repl/set-refresh-dirs "dev/src" "src" "test")
 
 (integrant.repl/set-prep! (comp duct/prep read-config))
--- dev/src/user.clj
+++ dev/src/user.clj
@@ -1,8 +1,16 @@
-(ns user)
+(ns user
+  (:refer-clojure :exclude [test]))
 
 (defn dev
   "Load and switch to the 'dev' namespace."
   []
   (require 'dev)
   (in-ns 'dev)
+  :loaded)
+
+(defn test
+  "Load and switch to the 'test' namespace."
+  []
+  (require 'test)
+  (in-ns 'test)
   :loaded)
