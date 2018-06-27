aea4acc5edac5b438fef4b72b28eeb10cabdf8c3
kbaba1001 <kbaba1001@users.noreply.github.com>
2018-06-28 00:16:10 +0900

    add assertion of db data

--- test/duct_api_sample/handler/users_test.clj
+++ test/duct_api_sample/handler/users_test.clj
@@ -2,45 +2,29 @@
   (:require [clojure.test :refer :all]
             [clojure.java.jdbc :as jdbc]
             [integrant.core :as ig]
-            [integrant.repl.state :refer [config system]]
-            [ring.mock.request :as mock]
-            [ataraxy.response :as response]
-            [duct-api-sample.handler.users :as users]))
-
-; (deftest smoke-test
-;   (testing "example page exists"
-;     (let [handler  (ig/init-key :duct-sample.handler/example {})
-;           response (handler (mock/request :get "/example"))]
-;       (is (= :ataraxy.response/ok (first response)) "response ok"))))
+            [integrant.repl.state :refer [config system]]))
 
 (defn db []
   (-> system (ig/find-derived-1 :duct.database/sql) val))
 
+(defn db-spec []
+  (:spec (db)))
+
 (defn clean-up-users [test-fn]
   (test-fn)
-  (jdbc/delete! (:spec (db)) :users []))
+  (jdbc/delete! (db-spec) :users []))
 
 (use-fixtures :each clean-up-users)
 
-
-; (deftest post-users
-;   (testing "POST /users"
-;     (let [handler (ig/init-key :duct-api-sample.handler.users/create {:db (db)
-;                                                                       :jwt-secret "xxxxxxxxxxxxxxxxxxxx"})
-;           response (handler (mock/request :post "/users" "\"email\": \"user1 @example.com\", \"password\": \"password\""))]
-;       (is (= {:status 201
-;               :headers {"content-type" "application/josn"}
-;               :body {:key "/users/1"}}
-;              response)))))
-
 (deftest post-users
+  ; boundaryのテストがあるのでdbをスタブしても良い気がする
   (testing "POST /users"
     (let [handler (ig/init-key :duct-api-sample.handler.users/create {:db (db)
                                                                       :jwt-secret "xxxxxxxxxxxxxxxxxxxx"})
           response (handler {:ataraxy/result [1 "user1@example.com" "password"]})]
       (is (= :ataraxy.response/created (first response)))
-      (is (re-find #"/users/\d+" (fnext response))))))
-
+      (is (re-find #"/users/\d+" (fnext response)))
+      (is (some? (first (jdbc/query (db-spec) ["SELECT TRUE FROM users WHERE email=?" "user1@example.com"])))))))
 
 
 ; 次の hoge の引数は :foo キーを持つ Map を1つ受け取る。
