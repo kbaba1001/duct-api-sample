121af9b64a20d70b5e6cccaa9f5f019419fa4c53
kbaba1001 <kbaba1001@users.noreply.github.com>
2018-06-27 22:59:12 +0900

    add duct-api-sample.handler.users-test

---
+++ test/duct_api_sample/handler/users_test.clj
@@ -0,0 +1,46 @@
+(ns duct-api-sample.handler.users-test
+  (:require [clojure.test :refer :all]
+            [integrant.core :as ig]
+            [integrant.repl.state :refer [config system]]
+            [ring.mock.request :as mock]
+            [ataraxy.response :as response]
+            [duct-api-sample.handler.users :as users]))
+
+; (deftest smoke-test
+;   (testing "example page exists"
+;     (let [handler  (ig/init-key :duct-sample.handler/example {})
+;           response (handler (mock/request :get "/example"))]
+;       (is (= :ataraxy.response/ok (first response)) "response ok"))))
+
+(defn db []
+  (-> system (ig/find-derived-1 :duct.database/sql) val))
+
+; (deftest post-users
+;   (testing "POST /users"
+;     (let [handler (ig/init-key :duct-api-sample.handler.users/create {:db (db)
+;                                                                       :jwt-secret "xxxxxxxxxxxxxxxxxxxx"})
+;           response (handler (mock/request :post "/users" "\"email\": \"user1 @example.com\", \"password\": \"password\""))]
+;       (is (= {:status 201
+;               :headers {"content-type" "application/josn"}
+;               :body {:key "/users/1"}}
+;              response)))))
+
+(deftest post-users
+  (testing "POST /users"
+    (let [handler (ig/init-key :duct-api-sample.handler.users/create {:db (db)
+                                                                      :jwt-secret "xxxxxxxxxxxxxxxxxxxx"})
+          response (handler {:ataraxy/result [1 "user1@example.com" "password"]})]
+      (is (= :ataraxy.response/created (first response)))
+      (is (re-find #"/users/\d+" (fnext response))))))
+
+
+
+; 次の hoge の引数は :foo キーを持つ Map を1つ受け取る。
+; :foo の value は要素数3の配列を期待しており、
+; この配列の添字1, 2 の位置の値を変数 email, password に
+; 代入してfunction内で使用する
+;
+; (defn hoge [{[_ email password] :foo}]
+;   (str email password))
+; (hoge {:foo [1 2 3]})
+; #=> "23"
