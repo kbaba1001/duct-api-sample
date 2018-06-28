5af9ebc20f7173926c0216ab526a841a82a2c02b
kbaba1001 <kbaba1001@users.noreply.github.com>
2018-06-28 16:43:58 +0900

    revert

--- dev/src/dev.clj
+++ dev/src/dev.clj
@@ -1,4 +1,5 @@
 (ns dev
+  (:refer-clojure :exclude [test])
   (:require [clojure.repl :refer :all]
             [fipp.edn :refer [pprint]]
             [clojure.tools.namespace.repl :refer [refresh]]
@@ -6,6 +7,7 @@
             [clojure.java.jdbc :as jdbc]
             [duct.core :as duct]
             [duct.core.repl :as duct-repl]
+            [eftest.runner :as eftest]
             [integrant.core :as ig]
             [integrant.repl :refer [clear halt go init prep reset]]
             [integrant.repl.state :refer [config system]]))
@@ -15,13 +17,16 @@
 (defn read-config []
   (duct/read-config (io/resource "dev.edn")))
 
+(defn test []
+  (eftest/run-tests (eftest/find-tests "test")))
+
 (defn db []
   (-> system (ig/find-derived-1 :duct.database/sql) val))
 
 (defn q [sql]
   (jdbc/query (:spec (db)) sql))
 
-(clojure.tools.namespace.repl/set-refresh-dirs "dev/src" "src")
+(clojure.tools.namespace.repl/set-refresh-dirs "dev/src" "src" "test")
 
 (when (io/resource "local.clj")
   (load "local"))
--- dev/src/user.clj
+++ dev/src/user.clj
@@ -1,16 +1,8 @@
-(ns user
-  (:refer-clojure :exclude [test]))
+(ns user)
 
 (defn dev
   "Load and switch to the 'dev' namespace."
   []
   (require 'dev)
   (in-ns 'dev)
-  :loaded)
-
-(defn test
-  "Load and switch to the 'test' namespace."
-  []
-  (require 'test)
-  (in-ns 'test)
   :loaded)
