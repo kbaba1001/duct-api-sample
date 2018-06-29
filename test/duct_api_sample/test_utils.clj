d3770998861d9efef4e1f6f27939a58370a65f23
kbaba1001 <kbaba1001@users.noreply.github.com>
2018-06-29 15:12:18 +0900

    Enabled to set environment variable of database

--- test/duct_api_sample/test_utils.clj
+++ test/duct_api_sample/test_utils.clj
@@ -1,12 +1,13 @@
 (ns duct-api-sample.test-utils
   (:require [duct.database.sql :refer :all]
+            [duct.core.env :as e]
             [clojure.java.jdbc :as jdbc]))
 
 (def db-spec
   {:dbtype "postgresql"
    :dbname "duct-api-sample-test"
-   :user "postgres"
-   :password ""})
+   :user (e/env ["DATABASE_POSTGRESQL_USERNAME" :or "postgres"])
+   :password (e/env ["DATABASE_POSTGRESQL_PASSWORD" :or ""])})
 
 (def db (->Boundary db-spec))
 
