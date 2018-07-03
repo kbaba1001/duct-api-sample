c6454d1591a0004566b40f23dd91587fd9a2730c
kbaba1001 <kbaba1001@users.noreply.github.com>
2018-07-03 11:35:00 +0900

    compact

--- src/duct_api_sample/handler/articles.clj
+++ src/duct_api_sample/handler/articles.clj
@@ -8,11 +8,9 @@
 ; curl -v -X POST -H "Content-Type: application/json" -H "Authorization: Token eyJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6ImJvYkBleGFtcGxlLmNvbSJ9.zA2uWGmbweddKXovU3lPgKwskd23Jn1QoS4My6yBVCE" -d '{"body": "hello world"}' http://localhost:3000/articles
 ; TODO 記事を作成できないケース
 (defmethod ig/init-key ::create [_ {:keys [db]}]
-  (fn [request]
+  (fn [{[_ body] :ataraxy/result {:keys [email]} :identity :as request}]
     (if-not (authenticated? request)
       (throw-unauthorized)
-      (let [user-email (-> request :identity :email)
-            user-id (:id (users/find-user-by-email db user-email))
-            body (-> request :body-params :body)
+      (let [user-id (:id (users/find-user-by-email db email))
             article-id (articles/create-article db user-id body)]
         [::response/created (str "/articles/" article-id)]))))
