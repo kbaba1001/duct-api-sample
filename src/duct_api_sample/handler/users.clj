a818b4097e677c7a17267115f6af8c8df16d0998
kbaba1001 <kbaba1001@users.noreply.github.com>
2018-07-07 21:49:43 +0900

    add user/create validation

--- src/duct_api_sample/handler/users.clj
+++ src/duct_api_sample/handler/users.clj
@@ -1,14 +1,23 @@
 (ns duct-api-sample.handler.users
-  (:require duct.database.sql
-            [ataraxy.response :as response]
+  (:require [ataraxy.response :as response]
+            [struct.core :as st]
             [integrant.core :as ig]
             [buddy.sign.jwt :as jwt]
             [duct-api-sample.boundary.users :as users]))
 
+; TODO emailは重複禁止にしたい。DBにもユニーク制約付けてなかった...
+(def create-form-schema
+  {:email [st/required st/email]
+   :password [st/required st/string [st/min-count 8] [st/max-count 100]]})
+
+; TODO body-paramsでインタフェースを統一したい
+; TODO add tests
 (defmethod ig/init-key ::create [_ {:keys [db]}]
-  (fn [{[_ email password] :ataraxy/result}]
-    (let [id (users/create-user db email password)]
-      [::response/created (str "/users/" id)])))
+  (fn [{[_ email password] :ataraxy/result body-params :body-params}]
+    (if-let [errors (first (st/validate body-params create-form-schema))]
+      [::response/bad-request errors]
+      (let [id (users/create-user db email password)]
+        [::response/created (str "/users/" id)]))))
 
 (defmethod ig/init-key ::signin [_ {:keys [db jwt-secret]}]
   (fn [{[_ email password] :ataraxy/result}]
