7c40cc1710b0d1fb8933aa2129e0cf4b3b892c82
kbaba1001 <kbaba1001@users.noreply.github.com>
2018-07-24 19:11:29 +0900

    add update article handler

--- src/duct_api_sample/handler/articles.clj
+++ src/duct_api_sample/handler/articles.clj
@@ -6,7 +6,7 @@
             [duct-api-sample.boundary.users :as users]
             [duct-api-sample.boundary.articles :as articles]))
 
-(def create-form-schema
+(def form-schema
   {:body [st/required st/string [st/max-count 140]]})
 
 ; curl -v -X POST -H "Content-Type: application/json" -H "Authorization: Token eyJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6ImJvYkBleGFtcGxlLmNvbSJ9.zA2uWGmbweddKXovU3lPgKwskd23Jn1QoS4My6yBVCE" -d '{"body": "hello world"}' http://localhost:3000/articles
@@ -17,21 +17,18 @@
         :as request}]
     (if-not (authenticated? request)
       (throw-unauthorized)
-      (if-let [errors (first (st/validate params create-form-schema))]
+      (if-let [errors (first (st/validate params form-schema))]
         [::response/bad-request errors]
         (let [user-id (:id (users/find-user-by-email db email))
               article-id (articles/create-article db user-id body)]
           [::response/created (str "/articles/" article-id)])))))
 
-; (defmethod ig/init-key ::update [_ {:keys [db]}]
-;   (fn [{{article-id :id} :ataraxy/result :as request}]
-;     (println request)))
-
-; (if-not (authenticated? request)
-; (throw-unauthorized)
-; (if-let [errors (first (st/validate (:body-params request) create-form-schema))]
-;   [::response/bad-request errors]
-;   (let [user-id (:id (users/find-user-by-email db (get-in request (get-in request [:identity :email]))))]
-;     (if (articles/update-article db article-id (get-in request [:body-params :body]))
-;       [::response/no-content]
-;       [::response/not-found]))))))
+(defmethod ig/init-key ::update [_ {:keys [db]}]
+  (fn [{[_ article-id] :ataraxy/result :as request}]
+    (if-not (authenticated? request)
+      (throw-unauthorized)
+      (if-let [errors (first (st/validate (:body-params request) form-schema))]
+        [::response/bad-request errors]
+        (if (articles/update-article db article-id (get-in request [:body-params :body]))
+          [::response/no-content]
+          [::response/not-found])))))
