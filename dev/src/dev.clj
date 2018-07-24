b9061d17ea747a9028cfe1ed47733147974bda30
kbaba1001 <kbaba1001@users.noreply.github.com>
2018-07-24 15:42:31 +0900

    change db connection in dev

--- dev/src/dev.clj
+++ dev/src/dev.clj
@@ -1,6 +1,8 @@
 (ns dev
   (:refer-clojure :exclude [test])
-  (:require [clojure.repl :refer :all]
+  (:require [duct.database.sql :refer :all]
+            [duct.core.env :as e]
+            [clojure.repl :refer :all]
             [fipp.edn :refer [pprint]]
             [clojure.tools.namespace.repl :refer [refresh]]
             [clojure.java.io :as io]
@@ -21,11 +23,13 @@
   ([] (eftest/run-tests (eftest/find-tests "test")))
   ([vars] (eftest/run-tests vars)))
 
-(defn db []
-  (-> system (ig/find-derived-1 :duct.database/sql) val))
+(def db-spec
+  {:connection (jdbc/get-connection {:connection-uri (e/env "DEV_DB_URL")})})
+
+(def db (->Boundary db-spec))
 
 (defn q [sql]
-  (jdbc/query (:spec (db)) sql))
+  (jdbc/query db-spec sql))
 
 (clojure.tools.namespace.repl/set-refresh-dirs "dev/src" "src" "test")
 
